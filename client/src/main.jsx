b22527d32da0a55a558925e29c18cf7467f9e7aa
Ninad Bhoir <ninadbhoir010@gmail.com>
2024-03-09 14:46:54 +0530

    develop: added routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import { CssBaseline } from "@mui/material"
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <HelmetProvider>
+      <CssBaseline/>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
